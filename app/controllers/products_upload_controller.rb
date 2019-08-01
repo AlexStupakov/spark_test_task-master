79dde7107d71444f5587055da232b3bd1e76875d
Oleksandr Stupakov <stepan.alex@gmail.com>
2019-08-01 12:15:50 +0300

    added sidekiq

--- app/controllers/products_upload_controller.rb
+++ app/controllers/products_upload_controller.rb
@@ -14,26 +14,7 @@ class ProductsUploadController < ApplicationController
   def create
     @products_upload = ProductsUpload.new(products_upload_params)
     if @products_upload.save
-      categories_hash = Spree::ShippingCategory.pluck(:name, :id).to_h
-      CSV.foreach(@products_upload.csvfile.path, { :col_sep => ';',
-                                                   headers: true,
-                                                   skip_blanks: true }) do |row|
-        next if row.to_h.values.all? { |column| column.nil? || column.strip.empty? }
-        category_id =
-          categories_hash[row['category']] ||
-            categories_hash.merge(row['category'] =>
-                                  Spree::ShippingCategory.create(name: row['category']).id
-                                  ).values.last
-
-        product = Spree::Product.new( name: row['name'],
-                                      available_on: Time.parse(row['availability_date']),
-                                      slug: row['slug'],
-                                      description: row['description'],
-                                      shipping_category_id: category_id,
-                                      price: row['price'])
-        product.save
-        product.master.stock_items.first.update_attributes(count_on_hand: row['stock_total'])
-      end
+      ProductsImportWorker.perform_async(@products_upload.csvfile.path)
       redirect_to '/admin/products'
     else
       render :new
--- app/models/products_upload.rb
+++ app/models/products_upload.rb
@@ -2,7 +2,7 @@ class ProductsUpload < ApplicationRecord
   has_attached_file :csvfile
   validates :csvfile, presence: true
   validates_attachment :csvfile,
-                       content_type: { content_type: ['text/plain', 'text/csv', 'application/vnd.ms-excel'],
+                       content_type: { content_type: ['text/plain', 'text/csv'],
                                        message: "is not in CSV format" },
                        size: { less_than: 1.megabyte }
 end
